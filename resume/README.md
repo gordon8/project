531698fa5a7c51a6c4f02c77b5df0aa7c127145a
gordon8 <973540381@qq.com>
2016-11-20 12:37:12 +0800

    新增个人页面

---
+++ resume/README.md
@@ -0,0 +1,34 @@
+
+# 个人页面
+
+个人展示页面，采用响应式布局的设计，同时使用了 HTML5 和 CSS3 的新特性。
+[查看demo](https://gordon8.github.io/project/resume)
+
+## 制作过程
+
+### HTML结构部分
+
+整体结构主要有两部分，分别是：nav 和 main，其中main部分包括四个section，分别为：about、skills、projects和contact。页面整体结构主要使用HTML5新标签构成。
+
+### CSS样式部分
+
+1. 整体布局
+   采用响应式布局的设计，盒模型均采用了```box-sizing: border-box;```,各section的高度与浏览器窗口大小一致均为100%。
+2. nav部分
+   采用```position: fixed;```,当浏览器窗口小于900px时，隐藏主导航栏，显示导航栏图标。
+3. main部分
+   当浏览器窗口大于900px时，主要采用flex布局，使浏览器窗口大小发生改变时保持元素的相对位置和大小不变，同时减少了依赖浮动布局来实现元素位置的定义以及重置元素大小。同时使用transform、transition等属性实现部分动画特效。
+   当浏览器窗口小于900px时，将各部分的```display```属性改为```block```;同时设置```height:auto```，使文档自上而下以正常文档流形式布局。
+
+### javaScript部分
+
+主要包含两个功能：
+
+1. nav部分
+   通过切换导航栏```class:open```属性实现导航栏显示隐藏。同时，点击导航栏时， document 以指定速度滚动到目标位置。
+2. main部分
+   通过监听滚动事件，使各```section```的class在```current```和```leave```之间切换，来实现动画切换。
+
+详细实现过程可查看源代码。
+
+
---
+++ resume/js/app.js
@@ -0,0 +1,83 @@
+(function ($) {
+    var $page = $('.page');
+
+    //切换显示导航栏
+    $page.on('click', '>nav>.toggle', function () {
+        $(this).closest('nav').toggleClass('open');
+    });
+    //点击导航栏，将其隐藏
+    $page.on('click', '>nav>ol', function () {
+        $(this).closest('nav').removeClass('open');
+    });
+
+    //为导航栏添加click动画
+    $page.on('click', '>nav a', function (e) {
+        e.preventDefault();
+        var $a = $(this);
+        var target = $a.attr('href');
+        var $target = $(target);
+        scrollTo($target);
+    });
+
+    //获取导航栏清单
+    var $navList = $page.find('>nav li');
+    //获取section列表
+    var $sections = $page.find('>main>section');
+    var sectionTopList = [];
+    $.each($sections, function (index, section) {
+        var $section = $(section);
+        var top = $section.offset().top;
+        sectionTopList[index] = top;
+    });
+
+    //执行checkNav&addAnimation
+    $(window).on('scroll', function () {
+        checkNav();
+        addAnimation();
+    });
+    checkNav();
+    addAnimation();
+
+    //定义checkNav函数：检查$navList，为当前的页面对应的li的子节点a添加class:selected;并移除其他a链接的selected
+    function checkNav() {
+        var currentIndex = getCurrentIndex();
+        $navList.eq(currentIndex)
+        .children('a').addClass('selected').end()
+        .siblings().children('a').removeClass('selected');
+    }
+
+    //定义addAnimation函数：为当前页面添加class:current，离开时移除class:current，并添加class:leave
+    function addAnimation() {
+        var currentIndex = getCurrentIndex();
+        $sections.eq(currentIndex)
+        .addClass('current').removeClass('leave')
+        .siblings().removeClass('current').addClass('leave');
+    }
+
+    //定义getCurrentIndex函数：获取当前的scrollTop值，并与sectionTopList中的值对比，确定目前的index
+    function getCurrentIndex() {
+        var iTop = $('body').scrollTop();
+        var eyePositon = iTop + $(window).height() / 2;
+        var currentIndex = 0;
+        for (var i = 0; i < sectionTopList.length; i++) {
+            if (sectionTopList[i+1] === undefined) {
+                currentIndex = i;
+                break;
+            }
+            if (eyePositon >= sectionTopList[i] && eyePositon <= sectionTopList[i+1]) {
+                currentIndex = i;
+                break;
+            }
+        }
+        return currentIndex;
+    }
+
+    //定义scrollTo函数：获取目标元素距页面顶部的高度，并让页面滚动到指定位置。
+    function scrollTo($el) {
+        var iTop = $el.offset().top;
+        $('html, body').animate({
+            scrollTop: iTop
+        }, 500);
+    }
+
+})(jQuery);
